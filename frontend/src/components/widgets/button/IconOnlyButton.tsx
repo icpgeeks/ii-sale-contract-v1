272b4b239b19e85b55c68c4ab2fb7d2210df2843
amira-delonixa <amiradelonixa@outlook.com>
2026-03-04 18:05:00 +0200

    refactor: convert IconOnlyButton to forwardRef for improved ref handling

--- frontend/src/components/widgets/button/IconOnlyButton.tsx
+++ frontend/src/components/widgets/button/IconOnlyButton.tsx
@@ -1,5 +1,8 @@
 import {Button, type ButtonProps} from 'antd';
+import {forwardRef} from 'react';
 
-export const IconOnlyButton = (props: Omit<ButtonProps, 'children'>) => {
-    return <Button {...props} />;
-};
+export const IconOnlyButton = forwardRef<HTMLButtonElement, Omit<ButtonProps, 'children'>>((props, ref) => {
+    return <Button ref={ref} {...props} />;
+});
+
+IconOnlyButton.displayName = 'IconOnlyButton';
